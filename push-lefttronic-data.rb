c8db2198812f6df2e16c32089f07451c3d388a7d
Rory O’Kane <rory@neomindlabs.com>
2012-06-18 17:14:50 -0400

    map projects to widgets by name instead of id number
    This way, it’s easier to update the code if the projects are changed.
    
    Now the only use of the id_nums is to update the project in a consistent order, and for debugging.

--- push-lefttronic-data.rb
+++ push-lefttronic-data.rb
@@ -5,13 +5,13 @@ require './bigtuna-ci-project-statuses'
 
 require 'leftronic'
 
-build_project_id_widgets = {
-	1 => 'ESlemK7N', # Project A
-	2 => 'WhsAHBM2', # Project B
-	3 => '9avuzLrP', # Project C
-	4 => '9U41swTM', # Project D
-	5 => 'QAWwnzHe', # Project E
-	6 => 'sqALqaQ6', # Project F
+build_project_name_widgets = {
+	"Project A" => 'ESlemK7N',
+	"Project B" => 'WhsAHBM2',
+	"Project C" => '9avuzLrP',
+	"Project D" => '9U41swTM',
+	"Project E" => 'QAWwnzHe',
+	"Project F" => 'sqALqaQ6',
 }
 
 def build_status_to_rating(status)
@@ -28,6 +28,6 @@ updater = Leftronic.new 'redacted'
 project_statuses = get_project_statuses
 project_statuses.each do |status|
 	rating = build_status_to_rating( status[:build_status] )
-	widget_id = build_project_id_widgets[status[:id_num]]
+	widget_id = build_project_name_widgets[status[:name]]
 	updater.push_number widget_id, rating
 end
