c99b8474bcf7def61b6a0b7a83e43bc39795680d
Rory O’Kane <rory@neomindlabs.com>
2012-06-18 17:35:14 -0400

    organized dashboard data pusher for multiple sources better

---
+++ freckle-hours-worked.rb
@@ -0,0 +1,7 @@
+#!ruby
+# encoding: utf-8
+
+# calculate total hours worked for the given day
+def total_hours_worked(day)
+	
+end
--- push-lefttronic-data.rb
+++ push-lefttronic-data.rb
@@ -2,30 +2,41 @@
 # encoding: utf-8
 
 require './bigtuna-ci-project-statuses'
+require './freckle-hours-worked'
 
 require 'leftronic'
 
-build_project_name_widget_ids = {
-	"Project A" => 'ESlemK7N',
-	"Project B" => 'WhsAHBM2',
-	"Project C" => '9avuzLrP',
-	"Project D" => '9U41swTM',
-	"Project E" => 'QAWwnzHe',
-	"Project F" => 'sqALqaQ6',
-}
+def update_build_statuses(updater)
+	build_project_name_widget_ids = {
+		"Project A" => 'ESlemK7N',
+		"Project B" => 'WhsAHBM2',
+		"Project C" => '9avuzLrP',
+		"Project D" => '9U41swTM',
+		"Project E" => 'QAWwnzHe',
+		"Project F" => 'sqALqaQ6',
+	}
 
-build_status_ratings = {
-	failed: 100,
-	in_progress: 50,
-	ok: 0,
-}
+	build_status_ratings = {
+		failed: 100,
+		in_progress: 50,
+		ok: 0,
+	} 
+
+	project_statuses = get_project_statuses
+	project_statuses.each do |status|
+		rating = build_status_ratings[ status[:build_status] ]
+		widget_id = build_project_name_widget_ids[ status[:name] ]
+		updater.push_number widget_id, rating
+	end
+end
+
+def update_freckle_hours(updater)
+	freckle_widget_id = 'IriiyIFP'
+	
+	# updater.push_number widget_id, a_number
+end
 
 access_key = 'redacted'
 updater = Leftronic.new access_key
-
-project_statuses = get_project_statuses
-project_statuses.each do |status|
-	rating = build_status_ratings[ status[:build_status] ]
-	widget_id = build_project_name_widget_ids[ status[:name] ]
-	updater.push_number widget_id, rating
-end
+# update_build_statuses(updater)
+update_freckle_hours(updater)
