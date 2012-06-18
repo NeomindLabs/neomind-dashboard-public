728c1e595a8c3e500a113b9801d804dd68b5256c
Rory O’Kane <rory@neomindlabs.com>
2012-06-18 16:23:44 -0400

    wrapped code in method get_project_statuses

--- bigtuna-ci-project-statuses.rb
+++ bigtuna-ci-project-statuses.rb
@@ -5,23 +5,26 @@ require 'nokogiri'
 require 'open-uri'
 require 'pp'
 
-url = "redacted"
-doc = Nokogiri::HTML(open(url))
-projects = doc.css('.project')
+def get_project_statuses
+	url = "redacted"
+	doc = Nokogiri::HTML(open(url))
+	projects = doc.css('.project')
 
-project_statuses = projects.map do |project|
-	
-	id = project.attr('id') #=> "project_6"
-	id_num = id.match(/\d+/).to_s.to_i
-	
-	name = project.at_css('h3 a').content.strip
-	
-	classes = project.attr('class').split #=> "status_build_failed project"
-	build_status_class = classes[classes.find_index { |klass| klass.start_with?('status_build_') }]
-	status = build_status_class.sub('status_build_', '').to_sym
-	
-	{id_num: id_num, name: name, build_status: status}
+	project_statuses = projects.map do |project|
+		
+		id = project.attr('id') #=> "project_6"
+		id_num = id.match(/\d+/).to_s.to_i
+		
+		name = project.at_css('h3 a').content.strip
+		
+		classes = project.attr('class').split #=> "status_build_failed project"
+		build_status_class = classes[classes.find_index { |klass| klass.start_with?('status_build_') }]
+		status = build_status_class.sub('status_build_', '').to_sym
+		
+		{id_num: id_num, name: name, build_status: status}
+	end
+	return project_statuses.sort_by { |status| status[:id_num] }
 end
-project_statuses.sort_by! { |status| status[:id_num] }
 
+project_statuses = get_project_statuses
 pp project_statuses
