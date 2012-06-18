d5070d0b8fcec51c3745d54a09e855e6d3ac4d6a
Rory O’Kane <rory@neomindlabs.com>
2012-06-18 16:14:10 -0400

    added code to scrape CI build statuses from BigTuna
    very simple, but it works – at least, when the page looks like it does currently

---
+++ bigtuna-ci-project-statuses.rb
@@ -0,0 +1,27 @@
+#!ruby
+# encoding: utf-8
+
+require 'nokogiri'
+require 'open-uri'
+require 'pp'
+
+url = "redacted"
+doc = Nokogiri::HTML(open(url))
+projects = doc.css('.project')
+
+project_statuses = projects.map do |project|
+	
+	id = project.attr('id') #=> "project_6"
+	id_num = id.match(/\d+/).to_s
+	
+	name = project.at_css('h3 a').content
+	
+	classes = project.attr('class').split #=> "status_build_failed project"
+	build_status_class = classes[classes.find_index { |klass| klass.start_with?('status_build_') }]
+	status = build_status_class.sub('status_build_', '').to_sym
+	
+	{id_num: id_num, name: name, build_status: status}
+end
+project_statuses.sort_by! { |status| status[:id_num] }
+
+pp project_statuses
