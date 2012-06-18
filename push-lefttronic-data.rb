9921a772ed87116668717c831be5bfd8da088e18
Rory O’Kane <rory@neomindlabs.com>
2012-06-18 17:18:12 -0400

    refactoring – naming, use hashes consistently

--- push-lefttronic-data.rb
+++ push-lefttronic-data.rb
@@ -5,7 +5,7 @@ require './bigtuna-ci-project-statuses'
 
 require 'leftronic'
 
-build_project_name_widgets = {
+build_project_name_widget_ids = {
 	"Project A" => 'ESlemK7N',
 	"Project B" => 'WhsAHBM2',
 	"Project C" => '9avuzLrP',
@@ -14,20 +14,18 @@ build_project_name_widgets = {
 	"Project F" => 'sqALqaQ6',
 }
 
-def build_status_to_rating(status)
-	status_ratings = {
-		failed: 100,
-		in_progress: 50,
-		ok: 0,
-	}
-	return status_ratings[status]
-end
+build_status_ratings = {
+	failed: 100,
+	in_progress: 50,
+	ok: 0,
+}
 
-updater = Leftronic.new 'redacted'
+access_key = 'redacted'
+updater = Leftronic.new access_key
 
 project_statuses = get_project_statuses
 project_statuses.each do |status|
-	rating = build_status_to_rating( status[:build_status] )
-	widget_id = build_project_name_widgets[status[:name]]
+	rating = build_status_ratings[ status[:build_status] ]
+	widget_id = build_project_name_widget_ids[ status[:name] ]
 	updater.push_number widget_id, rating
 end
