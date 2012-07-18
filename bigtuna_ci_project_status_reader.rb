b9ccab75fcd99a836b4f5e23b9a3a05e9ec6b95e
Rory O’Kane <rory@neomindlabs.com>
2012-07-18 12:32:06 -0400

    refactoring in BigTuna CI scraper
    extracted local variable `build_status_class_common_prefix`
    
    added comment about the implementation
    
    Changed #[]#find_index to just #find ( http://www.ruby-doc.org/core-1.9.3/Enumerable.html#method-i-find ). I couldn’t find the name of that method when I first wrote that code.

--- bigtuna_ci_project_status_reader.rb
+++ bigtuna_ci_project_status_reader.rb
@@ -10,6 +10,8 @@ class BigtunaCiProjectStatusReader
 	
 	# get project basic info and build statuses (all tests pass, some tests failed, in build queue, etc.)
 	def get_statuses
+		# scrape the statuses web page (more convenient than looking up an API)
+		
 		url = CONFIG["statuses URL"]
 		doc = Nokogiri::HTML(open(url))
 		projects = doc.css('.project')
@@ -25,8 +27,9 @@ class BigtunaCiProjectStatusReader
 				end,
 				build_status: begin
 					classes = project.attr('class').split #=> "status_build_failed project"
-					build_status_class = classes[classes.find_index { |klass| klass.start_with?('status_') }]
-					build_status_class.sub('status_', '').to_sym
+					build_status_class_common_prefix = 'status_'
+					build_status_class = classes.find { |klass| klass.start_with?(build_status_class_common_prefix) }
+					build_status_class.sub(build_status_class_common_prefix, '').to_sym
 				end,
 			}
 		end
