9f428a59d30e0ca55c7ff79888ecce32d26f6c66
Rory O’Kane <rory@neomindlabs.com>
2012-07-20 11:36:06 -0400

    renamed “rating” to “color” to reflect new name of hash
    build_status_colors used to be build_status_ratings

--- ci_build_status_updater.rb
+++ ci_build_status_updater.rb
@@ -26,9 +26,9 @@ class CiBuildStatusUpdater
 		project_statuses = @ci_build_status_reader.get_statuses
 		project_statuses.each do |status|
 			build_status = status[:build_status]
-			rating = build_status_colors[build_status]
-			raise "unknown build status “#{build_status}”" if rating.nil?
-			number = STOPLIGHT_COLOR_NUMBERS[rating]
+			color = build_status_colors[build_status]
+			raise "unknown build status “#{build_status}”" if color.nil?
+			number = STOPLIGHT_COLOR_NUMBERS[color]
 			
 			project_name = status[:name]
 			stream_name = project_name_stream_names[project_name]
